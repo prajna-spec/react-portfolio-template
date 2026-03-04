fe4cfb2f33daf3e64c91a8eca816a1c573d2e21c
prajna-spec <artisticprajna@gmail.com>
2026-03-04 06:47:58 +0530

    minor adjustments

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,6 +5,10 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
 import TextField from '@mui/material/TextField';
+import { LinkedIn } from '@mui/icons-material';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+
 
 function Contact() {
 
@@ -54,59 +58,9 @@ function Contact() {
       <div className="items-container">
         <div className="contact_wrapper">
           <h1>Contact Me</h1>
-          <p>Got a project waiting to be realized? Let's collaborate and make it happen!</p>
-          <Box
-            ref={form}
-            component="form"
-            noValidate
-            autoComplete="off"
-            className='contact-form'
-          >
-            <div className='form-flex'>
-              <TextField
-                required
-                id="outlined-required"
-                label="Your Name"
-                placeholder="What's your name?"
-                value={name}
-                onChange={(e) => {
-                  setName(e.target.value);
-                }}
-                error={nameError}
-                helperText={nameError ? "Please enter your name" : ""}
-              />
-              <TextField
-                required
-                id="outlined-required"
-                label="Email / Phone"
-                placeholder="How can I reach you?"
-                value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-                error={emailError}
-                helperText={emailError ? "Please enter your email or phone number" : ""}
-              />
-            </div>
-            <TextField
-              required
-              id="outlined-multiline-static"
-              label="Message"
-              placeholder="Send me any inquiries or questions"
-              multiline
-              rows={10}
-              className="body-form"
-              value={message}
-              onChange={(e) => {
-                setMessage(e.target.value);
-              }}
-              error={messageError}
-              helperText={messageError ? "Please enter the message" : ""}
-            />
-            <Button variant="contained" endIcon={<SendIcon />} onClick={sendEmail}>
-              Send
-            </Button>
-          </Box>
+          <p><i>Got a project waiting to be realized? Let's collaborate and make it happen! Please reach out here: {' '}</i>
+          <a href="https://www.linkedin.com/in/prajnagupta/" target="_blank" rel="noreferrer"><LinkedInIcon fontSize='medium'/></a>
+          </p>
         </div>
       </div>
     </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,6 @@ import '../assets/styles/Footer.scss'
 function Footer() {
   return (
     <footer>
-      <div>
-        <a href="www.linkedin.com/in/prajnagupta" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
-      </div>
       <p>A portfolio designed & built by <a href= "https://github.com/prajna-spec" target="_blank" rel="noreferrer">Prajna Gupta</a> with 💜</p>
     </footer>
   );
